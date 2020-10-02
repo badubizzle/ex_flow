e2be56ffd1cd605ea08502e5baa5183af644f6ea
badubizzle <badu.boahen@gmail.com>
2020-10-02 18:13:50 -0500

    Fix tests

--- test/ex_flow_web/live/page_live_test.exs
+++ test/ex_flow_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule ExFlowWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "DAG"
+    assert render(page_live) =~ "DAG"
   end
 end
