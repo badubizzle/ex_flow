c3790007663075c66a438d7d732551a79094d5c8
badubizzle <badu.boahen@gmail.com>
2020-11-18 23:46:48 -0600

    Add dag and task handler modules

---
+++ lib/ex_flow/dag_handler.ex
@@ -0,0 +1,20 @@
+defmodule ExFlow.DAGHandler do
+  @behaviour ExDag.DAG.Handlers.DAGHandler
+
+  @impl true
+  def on_dag_completed(%ExDag.DAGRun{}=dag_run) do
+
+    ExDag.DAG.Utils.print_status(dag_run.dag)
+    ExDag.DAG.Utils.print_task_runs(dag_run.dag.task_runs)
+    ExDag.Store.save_dag_run(dag_run)
+    ExFlow.Notifications.emit_dag_status(dag_run)
+  end
+
+  @impl true
+  def on_task_completed(%ExDag.DAGRun{}=dag_run, _task, _result) do
+    ExDag.DAG.Utils.print_status(dag_run.dag)
+    ExDag.DAG.Utils.print_task_runs(dag_run.dag.task_runs)
+    ExDag.Store.save_dag_run(dag_run)
+    ExFlow.Notifications.emit_dag_status(dag_run)
+  end
+end
