16ed21e1e133c6d8187a7d2faed2ea1f2024e271
badubizzle <badu.boahen@gmail.com>
2020-11-18 23:50:52 -0600

    Add presence tracker module

---
+++ lib/ex_flow/tracker.ex
@@ -0,0 +1,16 @@
+defmodule ExFlow.Tracker do
+
+  @tracker_name ExDag.Tracker
+
+  @spec track(topic:: binary(), key:: atom() | binary(), meta :: map()) :: {:ok, ref :: binary} | {:error, reason :: term}
+  def track(topic, key, meta \\ %{} ) do
+    Phoenix.Tracker.track(@tracker_name, self(), topic, key, meta)
+  end
+
+  def track_dag(dag) do
+    topic = "dags"
+    key = "#{dag.dag_id}"
+    track(topic, key, %{})
+  end
+
+end
