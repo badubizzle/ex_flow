8d8ea53b73b7e567deb833612905c2cb092aaf62
badubizzle <badu.boahen@gmail.com>
2020-11-18 23:45:28 -0600

    Add credo and test watch

--- mix.exs
+++ mix.exs
@@ -44,6 +44,9 @@ defmodule ExFlow.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+
       {:ex_dag, git: "https://github.com/badubizzle/ex_dag.git"}
     ]
   end
